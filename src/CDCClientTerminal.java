30789651e4cb5d6160bd38956b1a121223654522
dbestdan <dbestdan@gmail.com>
2017-03-03 20:54:11 +0100

    some modification in connection

--- src/CDCClientTerminal.java
+++ src/CDCClientTerminal.java
@@ -1,11 +1,46 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class CDCClientTerminal {
+	
+	private String databaseUrl ="";
+	private String userName ="";
+	private String password ="";
+	private String driver ="";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new CDCClientTerminal();
+		
+	}
+	
+	public CDCClientTerminal(){
 		Properties prop = new Properties();
-		prop.
+		try{
+			prop.load(new FileInputStream(System.getProperty("prop")));
+		} catch (IOException e) {
+			System.out.println("Couldn't load property file");
+		}
+		
+		databaseUrl = prop.getProperty("databaseUrl");
+		userName = prop.getProperty("userName");
+		password = prop.getProperty("password");
+		driver = prop.getProperty("driver");
+		try {
+			Connection conn = CDCConnection.getConnection(databaseUrl, driver, userName, password);
+			if(conn!=null){
+				System.out.println("connection successful");
+			}
+		} catch (SQLException se){
+			System.out.println("Couldn't connect to the database");
+			se.printStackTrace();
+		} catch (ClassNotFoundException cnfe){
+			System.out.println("Unable to load database driver");
+			cnfe.printStackTrace();
+		}
 	}
 
 }
--- src/CDCConnection.java
+++ src/CDCConnection.java
@@ -5,12 +5,13 @@ import java.util.Properties;
 
 public class CDCConnection {
 
-	public static Connection getConnection(String serverUrl, String userName, String password) throws SQLException {
+	public static Connection getConnection(String databaseUrl, String driver,String userName, String password) throws SQLException, ClassNotFoundException {
 		Connection conn = null;
 		Properties connectionProps = new Properties();
 		connectionProps.put("user", userName);
 		connectionProps.put("password", password);
-		conn = DriverManager.getConnection(serverUrl,connectionProps);
+		Class.forName(driver);
+		conn = DriverManager.getConnection(databaseUrl,connectionProps);
 		return conn;
 				
 	}
