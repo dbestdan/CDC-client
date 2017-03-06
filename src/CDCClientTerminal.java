7abf77b16c3dab981b154a2e05c49e91e41872c5
dbestdan <dbestdan@gmail.com>
2017-03-06 20:44:42 +0100

    on progress

--- src/CDCClientTerminal.java
+++ src/CDCClientTerminal.java
@@ -1,15 +1,20 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class CDCClientTerminal {
+public class CDCClientTerminal implements Runnable {
 	
 	private String databaseUrl ="";
 	private String userName ="";
 	private String password ="";
 	private String driver ="";
+	private String latestRecordCsvFile ="";
+	private Long latestRecordIdFromCsv;
+	private Long latestRecordIdFromDatabase;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -29,11 +34,16 @@ public class CDCClientTerminal {
 		userName = prop.getProperty("userName");
 		password = prop.getProperty("password");
 		driver = prop.getProperty("driver");
+		latestRecordCsvFile = prop.getProperty("latestRecordCsvFile");
 		try {
 			Connection conn = CDCConnection.getConnection(databaseUrl, driver, userName, password);
 			if(conn!=null){
 				System.out.println("connection successful");
 			}
+			latestRecordIdFromCsv = getLatestRecordIdFromCsv(latestRecordCsvFile);
+			latestRecordIdFromDatabase = CDCConnection.getLatestRecordIdFromDatabase(conn);
+			System.out.println("csv: "+ latestRecordIdFromCsv);
+			System.out.println("database: " + latestRecordIdFromCsv);
 		} catch (SQLException se){
 			System.out.println("Couldn't connect to the database");
 			se.printStackTrace();
@@ -41,6 +51,50 @@ public class CDCClientTerminal {
 			System.out.println("Unable to load database driver");
 			cnfe.printStackTrace();
 		}
-	}
+		
+		
 
+	}
+	
+	public void run(){
+		try {
+			Connection conn = CDCConnection.getConnection(databaseUrl, driver, userName, password);
+	
+		}catch (SQLException se){
+			System.out.println("Couldn't connect to the database");
+			se.printStackTrace();
+		}catch (ClassNotFoundException cnfe){
+			System.out.println("Unable to load database driver");
+			cnfe.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * Function to return latest id of the record 
+	 * migrated to data ware house
+	 * 
+	 */
+
+	public Long getLatestRecordIdFromCsv(String fileUrl){
+        String line = "";
+        String cvsSplitBy = ",";
+        Long recordId = new Long(0);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(latestRecordCsvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] changedRecord = line.split(cvsSplitBy);
+                recordId = new Long(changedRecord[1]);               
+
+            }
+
+        } catch (IOException e) {
+        	System.out.println("Couldn't read CSV file");
+            e.printStackTrace();
+        }
+        return recordId;
+	}
 }
--- src/CDCConnection.java
+++ src/CDCConnection.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class CDCConnection {
@@ -14,5 +16,27 @@ public class CDCConnection {
 		conn = DriverManager.getConnection(databaseUrl,connectionProps);
 		return conn;
 				
+	}
+	
+	/**
+	 * Function to return the latest id of 
+	 * the changed record present in the database 
+	 * @param conn
+	 * @return latestIdFromDatabase
+	 */
+	public static Long getLatestRecordIdFromDatabase(Connection conn){
+		Long latestIdFromDatabase = new Long(0);
+		try{
+			Statement stmt = conn.createStatement();
+			String query = "SELECT MAX(event_id) FROM audit.logged_actions";
+			ResultSet rs = stmt.executeQuery(query);
+			if(rs.next()){
+				latestIdFromDatabase = rs.getLong(1);
+			}
+		}catch(Exception sqle){
+			System.out.println("Couldn't get latest record id from the database");
+			sqle.printStackTrace();
+		}
+		return latestIdFromDatabase;
 	}
 }
